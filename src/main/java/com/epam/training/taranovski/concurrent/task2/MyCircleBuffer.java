c9b3ff79db241d0af4b9ff6a3ca043f135fee9ff
user <user@U3>
2014-08-06 09:16:53 +0300

    1 task 100% 4 task 100% 5 task 100% 2 task 20%

--- src/main/java/com/epam/training/taranovski/concurrent/task2/MyCircleBuffer.java
+++ src/main/java/com/epam/training/taranovski/concurrent/task2/MyCircleBuffer.java
@@ -3,13 +3,69 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.epam.training.taranovski.concurrent.task2;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author user
+ * @param <T>
  */
-public class MyCircleBuffer {
-    
+public class MyCircleBuffer<T> {
+
+    private int current;
+    private int size;
+    private Object[] buffer;
+
+    /**
+     *
+     * @param size
+     */
+    public MyCircleBuffer(int size) {
+        this.size = size;
+        current = 0;
+        buffer = new Object[size];
+    }
+
+    /**
+     *
+     * @param item
+     */
+    public synchronized void put(T item) {
+        if (item == null) {
+            throw new RuntimeException("cannot put a null into a buffer");
+        }
+        while (buffer[current] != null) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(MyCircleBuffer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        buffer[current] = item;
+        current = (current + 1) % size;
+        notifyAll();
+    }
+
+    /**
+     *
+     * @return
+     */
+    public synchronized T get() {
+        while (buffer[current] == null) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(MyCircleBuffer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        T item = (T) buffer[current];
+        buffer[current] = null;
+        current = (current + 1) % size;
+        notifyAll();
+        return item;
+    }
+
 }
